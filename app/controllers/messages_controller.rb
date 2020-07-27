3ddf591e4e9ec38c0eee658ccc1cd0884c540c3c
m-mitsui <paperclip321@icloud.com>
2020-07-27 14:43:27 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
+  
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if  @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+      # 保存できた場合とできない場合indexアクションが実行される処理を分岐している
+  end 
+  # @roomからidを取得しそれを@messageで呼び出し保存している
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
