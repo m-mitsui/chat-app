4fa5a40f51d7f293a0a2cda4c99eaba4e174a08b
m-mitsui <paperclip321@icloud.com>
2020-07-27 12:45:10 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
-
+  has_many :messages
   validates :names, presence: true
 end   #ルームが存在している場合だけ作成可能と設定を設けている
