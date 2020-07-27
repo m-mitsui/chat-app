a63f1d3ba7e364b43f8ea94100108f9a4c00b2ff
m-mitsui <paperclip321@icloud.com>
2020-07-27 14:59:58 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)  #一覧画面でメッセージ情報を取得する設定
   end
   
   def create
