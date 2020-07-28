910a65e5a05a029f82d170aa6d8d705bdfac1961
m-mitsui <paperclip321@icloud.com>
2020-07-28 17:10:19 +0900

    各モデルのテスト

--- config/application.rb
+++ config/application.rb
@@ -9,17 +9,21 @@ Bundler.require(*Rails.groups)
 module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.i18n.default_locale = :ja
-    config.time_zone = 'Tokyo'
+    config.load_defaults 6.0
+    config.i18n.default_locale = :en
+  
+    # config.i18n.default_locale = :ja
+    # config.time_zone = 'Tokyo'
+    
+      # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
     end
-      # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
